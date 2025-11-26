462971e6323cbf222a244ec93f5f1832448ff67c
Zach Baker <ZEBaker98@gmail.com>
2025-11-26 12:22:00 -0600

    construct from address control pin values

--- src/adg729.rs
+++ src/adg729.rs
@@ -4,6 +4,12 @@ pub struct ADG729<I2C> {
     enabled: u8,
 }
 
+impl<I2C> ADG729<I2C> {
+    fn address_from_pins(a0: bool, a1: bool) -> u8 {
+        0b1001100 | (a0 as u8) | ((a1 as u8) << 1)
+    }
+}
+
 impl<I2C> ADG729<I2C>
 where
     I2C: embedded_hal::i2c::I2c,
@@ -14,6 +20,10 @@ where
             address,
             enabled: 0,
         }
+    }
+
+    pub fn from_address_pins(i2c: I2C, a0: bool, a1: bool) -> Self {
+        Self::new(i2c, Self::address_from_pins(a0, a1))
     }
 
     pub fn set_channels(&mut self, enabled: u8) -> Result<(), I2C::Error> {
@@ -58,6 +68,10 @@ where
             address,
             enabled: 0,
         }
+    }
+
+    pub fn from_address_pins_async(i2c: I2C, a0: bool, a1: bool) -> Self {
+        Self::new_async(i2c, Self::address_from_pins(a0, a1))
     }
 
     pub async fn set_channels_async(&mut self, enabled: u8) -> Result<(), I2C::Error> {
